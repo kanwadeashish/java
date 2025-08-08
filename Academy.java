09bdf985ee0bde5adfd61281a40a4407eeac657b
ashishkanwade <ashishkanwade1008@gmail.com>
2025-08-08 16:13:14 +0530

    I have added a java class.

---
+++ Academy.java
@@ -0,0 +1,16 @@
+class Academy{
+public static void main(String[]args){
+
+String principal="Bagane";
+String vicePrincipal="Rokade";
+String headOfDepartment="Jadhav";
+String teacher="Kothali";
+String peon="Mule";
+
+System.out.println("principal="+principal);
+System.out.println("vicePrincipal="+vicePrincipal);
+System.out.println("headOfDepartment="+headOfDepartment);
+System.out.println("teacher="+teacher);
+System.out.println("peon="+peon);
+}
+}
