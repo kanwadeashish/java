e61535389559a8b3f77bd8c128d6a5ac75967c56
kanwadeashish <ashish.xworkz@gmail.com>
2025-08-25 17:19:55 +0530

    Daily java practice files.

---
+++ Variables/Science.java
@@ -0,0 +1,13 @@
+class Science{
+	
+	static int id=1;
+	static String name="Ajay";
+	static byte serialNumber=123;
+	static short collegeFee=12345;
+	static long phoneNumber=123456789L;
+	static float result=88.88f;
+	static double crieteria=99.99;
+	static char grade='b';
+	static boolean isAdmitted=false;
+	
+}
---
+++ Variables/ScienceRunner.java
@@ -0,0 +1,25 @@
+class ScienceRunner{
+	
+	public static void main(String[]args){
+	
+	System.out.println("id="+Science.id);
+	
+	System.out.println("name="+Science.name);
+	
+	System.out.println("serialNumber="+Science.serialNumber);
+	
+	System.out.println("collegeFee="+Science.collegeFee);
+	
+	System.out.println("phoneNumber="+Science.phoneNumber);
+	
+	System.out.println("result="+Science.result);
+	
+	System.out.println("crieteria="+Science.crieteria);
+	
+	System.out.println("grade="+Science.grade);
+	
+	System.out.println("isAdmitted="+Science.isAdmitted);
+	
+}
+
+}
