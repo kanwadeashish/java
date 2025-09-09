d0a47b759a5ad35f975a8f4026ccddf7018983a0
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-09 10:08:04 +0530

    Instance variables.

---
+++ Variables/Draco.java
@@ -0,0 +1,23 @@
+class Draco{
+	
+	public String name = "Draco";
+	public int id = 4;
+	public long phoneNumber = 432156789L;
+	public double percentage = 36.36;
+	public float overallResult = 90.14f;
+	public char rating = 'a';
+	public boolean isPassed = true;
+	
+	public void displayInfo(){
+		
+		System.out.println("Name = "+name);
+		System.out.println("Id = "+id);
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Percentage = "+percentage);
+		System.out.println("Overall Result = "+overallResult);
+		System.out.println("Rating = "+rating);
+		System.out.println("Is passed = "+isPassed);
+		
+	}
+	
+}
---
+++ Variables/DracoRunner.java
@@ -0,0 +1,11 @@
+class DracoRunner{
+	
+	public static void main(String[]args){
+		
+		Draco draco = new Draco();
+		
+		draco.displayInfo();
+		
+	}
+	
+}
