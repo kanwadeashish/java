12552c9a20e5bc060a5325ad5c4c025fd1651019
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-09 10:00:06 +0530

    Instance variables.

---
+++ Variables/Harry.java
@@ -0,0 +1,23 @@
+class Harry{
+	
+	public String name = "Harry";
+	public int id = 1;
+	public long phoneNumber = 123456789L;
+	public double percentage = 99.99;
+	public float overallResult = 90.11f;
+	public char rating = 'a';
+	public boolean isPassed = true;
+	
+	public void displayInfo(){
+		
+		System.out.println("Name = "+name);
+		System.out.println("Id = "+id);
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Percentage = "+percentage);
+		System.out.println("Overall Result = "+overallResult);
+		System.out.println("Rating = "+rating);
+		System.out.println("Is passed = "+isPassed);
+		
+	}
+	
+}
---
+++ Variables/HarryRunner.java
@@ -0,0 +1,11 @@
+class HarryRunner{
+	
+	public static void main(String[]args){
+		
+		Harry harry = new Harry();
+		
+		harry.displayInfo();
+		
+	}
+	
+}
