e717b430ff0a879a79a530b40a0da73ef5d5e7a9
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-11 19:34:39 +0530

    Instance methods.

---
+++ Methods/Lily.java
@@ -0,0 +1,46 @@
+class Lily{
+	
+	public String name = "Lily";
+	public int id = 1;
+	public long phoneNumber = 123456789L;
+	public double result = 99.99;
+	public float percentage = 98.98f;
+	public char rating = 'a';
+	public boolean isPassed = true;
+	
+	public String name(){
+		System.out.println(name);
+		return name;
+	}
+	
+	public int id(){
+		System.out.println(id);
+		return id;
+	}
+	
+	public long phoneNumber(){
+		System.out.println(phoneNumber);
+		return phoneNumber;
+	}
+	
+	public double result(){
+		System.out.println(result);
+		return result;
+	}
+	
+	public float percentage(){
+		System.out.println(percentage);
+		return percentage;
+	}
+	
+	public char rating(){
+		System.out.println(rating);
+		return rating;
+	}
+	
+	public boolean isPassed(){
+		System.out.println(isPassed);
+		return isPassed;
+	}
+	
+}
---
+++ Methods/LilyRunner.java
@@ -0,0 +1,17 @@
+class LilyRunner{
+	
+	public static void main(String[]args){
+		
+		Lily lily = new Lily();
+		
+		String name = lily.name();
+		int id = lily.id();
+		long phoneNumber= lily.phoneNumber();
+ 		double result=lily.result();
+		float percentage = lily.percentage();
+		char rating = lily.rating();
+		boolean isPassed = lily.isPassed();
+		
+	}
+	
+}
