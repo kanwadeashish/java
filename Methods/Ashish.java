0d5498e974e2fa54875d5e0c6125d346a8acbb65
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-13 11:23:29 +0530

    Instance methods with parameter.

---
+++ Methods/Ashish.java
@@ -0,0 +1,11 @@
+class Ashish{
+	
+	public void name(String surname){
+		
+		String name = "Ashish";
+		System.out.println("The name is = "+name);
+		System.out.println("The name is = "+surname);
+		
+	}
+	
+}
---
+++ Methods/AshishRunner.java
@@ -0,0 +1,9 @@
+class AshishRunner{
+	
+	public static void main(String [] args){
+		
+		Ashish ash = new Ashish();
+		ash.name("Kanwade");
+		}
+
+}
