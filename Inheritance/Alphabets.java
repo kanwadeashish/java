afa01b74a4056ebb5bfbf76ad803a058e8be8ebf
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-23 13:52:57 +0530

    Inheritance.

---
+++ Inheritance/Alphabets.java
@@ -0,0 +1,54 @@
+class Alphabets{
+	
+	public static void alphabetA(){
+		
+		System.out.println("This is alphabet A");
+		
+	}
+	public static void alphabetB(){
+		
+		System.out.println("This is alphabet B");
+		
+	}
+	public static void alphabetC(){
+		
+		System.out.println("This is alphabet C");
+		
+	}
+	public static void alphabetD(){
+		
+		System.out.println("This is alphabet D");
+		
+	}
+	public static void alphabetE(){
+		
+		System.out.println("This is alphabet E");
+		
+	}
+	public static void alphabetF(){
+		
+		System.out.println("This is alphabet F");
+		
+	}
+	public static void alphabetG(){
+		
+		System.out.println("This is alphabet G");
+		
+	}
+	public static void alphabetH(){
+		
+		System.out.println("This is alphabet H");
+		
+	}
+	public static void alphabetI(){
+		
+		System.out.println("This is alphabet I");
+		
+	}
+	public static void alphabetJ(){
+		
+		System.out.println("This is alphabet J");
+		
+	}
+	
+}
---
+++ Inheritance/CharactersRunner.java
@@ -0,0 +1,21 @@
+class CharactersRunner{
+	
+	public static void main(String[]args){
+		
+		//The static method can be called by using className or using refName after creating Instance
+		//In this class it is called by using className
+		
+		Characters.alphabetA();
+		Characters.alphabetB();
+		Characters.alphabetC();
+		Characters.alphabetD();
+		Characters.alphabetE();
+		Characters.alphabetF();
+		Characters.alphabetG();
+		Characters.alphabetH();
+		Characters.alphabetI();
+		Characters.alphabetJ();
+		
+	}
+	
+}
