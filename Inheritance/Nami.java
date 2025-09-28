7cbec5395c2824d626f389180a26a2d2e3d66796
kanwadeashish <ashish.xworkz@gmail.com>
2025-09-28 15:20:10 +0530

    this and super keyword are used.

---
+++ Inheritance/Nami.java
@@ -0,0 +1,71 @@
+class Nami{
+	
+	public Nami(){
+		
+		System.out.println("This is a non argumental constructor from SuperClass.");
+		
+	}
+	public Nami(int id){
+		
+		System.out.println("This is a argumental constructor with one argument from SuperClass.");
+		System.out.println("Id = "+id);
+		
+	}
+	public Nami(int id,String name){
+		
+		System.out.println("This is a argumental constructor with two argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		
+	}
+	public Nami(int id,String name,long phoneNumber){
+		
+		System.out.println("This is a argumental constructor with three argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		System.out.println("Phone Number = "+phoneNumber);	
+		
+	}
+	public Nami(int id,String name,long phoneNumber,double result){
+		
+		System.out.println("This is a argumental constructor with four argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Result = "+result);
+		
+	}
+	public Nami(int id,String name,long phoneNumber,double result,float percentage){
+		
+		System.out.println("This is a argumental constructor with five argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Result = "+result);
+		System.out.println("Percentage = "+percentage);
+		
+	}
+	public Nami(int id,String name,long phoneNumber,double result,float percentage,char rating){
+		
+		System.out.println("This is a argumental constructor with six argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Result = "+result);
+		System.out.println("Percentage = "+percentage);
+		System.out.println("Rating = "+rating);
+		
+	}
+	public Nami(int id,String name,long phoneNumber,double result,float percentage,char rating,boolean isPassed){
+		
+		System.out.println("This is a argumental constructor with Seven argument from SuperClass.");
+		System.out.println("Id = "+id);	
+		System.out.println("Name = "+name);	
+		System.out.println("Phone Number = "+phoneNumber);
+		System.out.println("Result = "+result);
+		System.out.println("Percentage = "+percentage);
+		System.out.println("Is passed = "+isPassed);
+		
+	}
+	
+}
---
+++ Inheritance/SanjiRunner.java
@@ -0,0 +1,16 @@
+class SanjiRunner{
+	
+	public static void main(String[]args){
+		
+		Sanji sanji = new Sanji();
+		Sanji one = new Sanji(2);
+		Sanji two = new Sanji(2,"Sanji");
+		Sanji three = new Sanji(2,"Sanji",678956789L);
+		Sanji four = new Sanji(2,"Sanji",678956789L,35.99);
+		Sanji five = new Sanji(2,"Sanji",678956789L,35.99,49.98f);
+		Sanji six = new Sanji(2,"Sanji",678956789L,35.99,49.98f,'d');
+		Sanji seven = new Sanji(2,"Sanji",678956789L,35.99,49.98f,'d',true);
+		
+	}
+	
+}
