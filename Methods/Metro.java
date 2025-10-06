421ed78f92c0f3a211c64a73163aedf9b230ef5d
kanwadeashish <ashish.xworkz@gmail.com>
2025-10-06 20:58:34 +0530

    method overloading.

---
+++ Methods/Metro.java
@@ -0,0 +1,19 @@
+class Metro{
+
+	public void projectInfo(String projectName){
+		
+		System.out.println("The name of the project is = "+projectName);
+		
+	}
+	public void projectInfo(int projectBudget){
+		
+		System.out.println("The budget of the project is = "+projectBudget);
+		
+	}
+	public void projectInfo(long projectCode){
+		
+		System.out.println("The code of the project is = "+projectCode);
+		
+	}
+
+}
---
+++ Methods/MetroRunner.java
@@ -0,0 +1,13 @@
+class MetroRunner{
+	
+	public static void main(String [] args){
+		
+		Metro metro = new Metro();
+		
+		metro.projectInfo("Metro");
+		metro.projectInfo(120120);
+		metro.projectInfo(123456789L);
+		
+	}
+	
+}
