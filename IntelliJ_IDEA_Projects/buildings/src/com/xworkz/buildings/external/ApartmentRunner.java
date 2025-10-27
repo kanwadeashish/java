5f5da03c653cadb7fe51bd7f751fc1891595eca7
kanwadeashish <ashish.xworkz@gmail.com>
2025-10-27 21:39:54 +0530

    Using custom datatypes array.

---
+++ IntelliJ_IDEA_Projects/buildings/src/com/xworkz/buildings/external/ApartmentRunner.java
@@ -0,0 +1,42 @@
+package com.xworkz.buildings.external;
+
+import com.xworkz.buildings.internal.Apartment;
+import com.xworkz.buildings.internal.Floors;
+import com.xworkz.buildings.internal.Rooms;
+
+public class ApartmentRunner {
+
+    public static void main(String [] args){
+
+        Apartment apartment=new Apartment();
+
+        Floors floor = new Floors();
+
+        Floors floor1=new Floors(1,"Apple");
+        Floors floor2=new Floors(2,"Ball");
+        Floors floor3=new Floors(3,"Cat");
+        Floors floor4=new Floors(4,"Dog");
+        Floors floor5=new Floors(5,"Empty");
+
+        Floors [] floors={floor1,floor2,floor3,floor4,floor5};
+
+        apartment.apartments(floors);
+
+        Rooms roomOne = new Rooms(1,"One",123456789L,8.8,'a',true);
+        Rooms roomTwo = new Rooms(2,"Two",223456789L,8.8,'a',true);
+        Rooms roomThree = new Rooms(3,"Three",323456789L,8.8,'a',true);
+        Rooms roomFour = new Rooms(4,"Four",423456789L,8.8,'a',true);
+        Rooms roomFive = new Rooms(5,"Five",523456789L,8.8,'a',true);
+        Rooms roomSix = new Rooms(6,"Six",623456789L,8.8,'a',true);
+        Rooms roomSeven = new Rooms(7,"Seven",723456789L,8.8,'a',true);
+        Rooms roomEight = new Rooms(8,"Eight",823456789L,8.8,'a',true);
+        Rooms roomNine = new Rooms(9,"Nine",923456789L,8.8,'a',true);
+        Rooms roomTen = new Rooms(10,"Ten",103456789L,8.8,'a',true);
+
+        Rooms [] rooms = {roomOne,roomTwo,roomThree,roomFour,roomFive,roomSix,roomSeven,roomEight,roomNine,roomTen};
+
+        floor.displayRooms(rooms);
+
+    }
+
+}
---
+++ IntelliJ_IDEA_Projects/buildings/src/com/xworkz/buildings/internal/Apartment.java
@@ -0,0 +1,15 @@
+package com.xworkz.buildings.internal;
+
+public class Apartment {
+
+    public void apartments(Floors[] floors){
+
+        for(int num=0; num<floors.length; num++){
+
+            Floors floorsDetails=floors[num];
+
+            floorsDetails. floorsInfo();
+        }
+    }
+
+}
